1b0cd078a936353bddb15cca3571894aa2e54a52
nemu <nemu@nemu-M1.local>
2025-12-16 18:57:03 +0900

    chore: add vanilla js global error handler

--- pages/_document.js
+++ pages/_document.js
@@ -12,6 +12,40 @@ export default function Document() {
                 <meta name="apple-mobile-web-app-title" content="BeerAlert" />
             </Head>
             <body>
+                <div id="safari-debug-log" style={{
+                    position: 'fixed', bottom: '150px', left: 0, right: 0,
+                    height: '150px', overflowY: 'auto',
+                    background: 'rgba(50,0,0,0.9)', color: '#fff',
+                    fontSize: '10px', padding: '5px', zIndex: 10000,
+                    pointerEvents: 'none', display: 'none'
+                }}></div>
+                <script dangerouslySetInnerHTML={{
+                    __html: `
+                        window.onerror = function(msg, url, line, col, error) {
+                            var log = document.getElementById('safari-debug-log');
+                            if (log) {
+                                log.style.display = 'block';
+                                var div = document.createElement('div');
+                                div.style.borderBottom = '1px solid #666';
+                                div.style.padding = '2px';
+                                div.textContent = 'Global Error: ' + msg + ' (' + line + ':' + col + ')';
+                                log.appendChild(div);
+                            }
+                        };
+                        // Also catch unhandled promise rejections if supported
+                        window.onunhandledrejection = function(event) {
+                             var log = document.getElementById('safari-debug-log');
+                             if (log) {
+                                log.style.display = 'block';
+                                var div = document.createElement('div');
+                                div.style.borderBottom = '1px solid #666';
+                                div.style.padding = '2px';
+                                div.textContent = 'Unhandled Rejection: ' + event.reason;
+                                log.appendChild(div);
+                             }
+                        };
+                    `
+                }} />
                 <Main />
                 <NextScript />
             </body>
