091e9c634861a8ddc711fa8bc441712c87d00eb1
nemu <nemu@nemu-M1.local>
2025-12-16 19:09:29 +0900

    fix: correct document structure

--- pages/_document.js
+++ pages/_document.js
@@ -3,11 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="ja">
-            <Head>
-                <body>
-                    <Main />
-                    <NextScript />
-                </body>
+            <Head />
+            <body>
+                <Main />
+                <NextScript />
+            </body>
         </Html>
     )
 }
