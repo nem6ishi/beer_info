964660ea1eeb44d381cc5a889a8e476d70670a7f
nemu <nemu@nemu-M1.local>
2026-05-09 16:44:49 +0900

    fix: update import path for scrape_brewery_details

--- backend/src/commands/enrich_breweries.py
+++ backend/src/commands/enrich_breweries.py
@@ -11,7 +11,7 @@ from dateutil import parser as dateutil_parser
 
 from ..core.db import get_supabase_client
 from ..core.types import UntappdBreweryDetails
-from ..services.untappd.searcher import scrape_brewery_details
+from ..services.untappd.http_client import scrape_brewery_details
 
 logger: logging.Logger = logging.getLogger(__name__)
 
